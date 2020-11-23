7de4d119071a0f0990c5623a0a3b04b93f919d0d
VINDOX <hagenimanemmanuel@gmail.com>
2020-11-23 19:57:39 +0200

    newexecises

---
+++ variables.js
@@ -0,0 +1,11 @@
+/*let name='vindo'
+let bonus=200
+let score=10
+let total=bonus+score
+console.log(total);*/
+
+let firstname='vindo'
+let lastname='kazina'
+console.log('your first name is: ',firstname);
+console.log('your last  name is: ',lastname);
+console.log('your full name are:',firstname +' ' +lastname);
