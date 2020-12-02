962346d7f7ed99403fa06620ac42523b84bc2fc9
VINDOX <hagenimanemmanuel@gmail.com>
2020-12-03 00:21:23 +0200

    anotherpage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var fern=prompt('please enter you fehrenheit number');
-console.log=(fern)
-alert('you have entered' +'   '+fern);
-var celci=(fern-32)*5/9;
-console.log=(celci);
-alert('your celsius is '+celci +'  '+ 'degree');
+let a='im globalThis'
+if(true){
+     a='kakaveyaw';
+     console.log(a);
+}
+
